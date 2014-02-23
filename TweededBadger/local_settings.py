a10ceb7ec66a94ef03f8ddfaf70dc88724688b1a
Will Jutsum <will@crabcreative.com>
2014-02-23 18:38:50 +0000

    init commit

---
+++ TweededBadger/local_settings.py
@@ -0,0 +1,12 @@
+
+
+database = {
+    'default': {
+       'ENGINE': 'django.db.backends.mysql',
+       'NAME': 'tweededbadger_local',
+       'USER': 'root',
+       'PASSWORD': '',
+       'HOST': '127.0.0.1',
+       'PORT': 3306,
+   }
+}
